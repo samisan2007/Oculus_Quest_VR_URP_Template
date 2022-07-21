b4ea166b9c94b5bf35e12287cfa19c5711c9b756
samisan2007 <44548656+samisan2007@users.noreply.github.com>
2022-07-21 12:26:32 +0300

    commit

---
+++ Assets/Scripts/QuitMGMT.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitMGMT : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            QuitGame();
+        }
+    }
+    public void QuitGame()
+    {
+        #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+                Application.Quit();
+    }
+}
